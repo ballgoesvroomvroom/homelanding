671b6d3d5d49c4d5ef2e6f5ee1bb01ce424eec41
whiteball <chongjayden07@gmail.com>
2022-11-29 23:10:55 +0800

    added 404 page added more content to landing page

--- server/includes/views.js
+++ server/includes/views.js
@@ -6,22 +6,8 @@ const html = path.join(root, "/public/html/")
 
 class Views {
 	static home = path.join(html, "base.html")
-	static login = path.join(html, "views/login.html")
-	static register = path.join(html, "views/register.html")
-
-	static create = path.join(html, "views/create.html")
-	static presets_create = path.join(html, "views/presets_create.html")
-
-	static study = path.join(html, "views/study.html")
-	static test = path.join(html, "views/questions.html")
-
-	static flashtabs_export = path.join(html, "views/flashtabs_export.html")
-
-	static settings = path.join(html, "views/settings.html")
 
 	static notFound = path.join(html, "includes/404.html")
-
-	static admin_panel = path.join(html, "views/admin/panel.html")
 }
 
 module.exports = Views;
