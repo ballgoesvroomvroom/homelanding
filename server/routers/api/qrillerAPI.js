f0ae933cc396bf5195b2dab04f6d5b2d31f63b96
whiteball <chongjayden07@gmail.com>
2023-05-23 20:41:51 +0800

    added answer API

--- server/routers/api/qrillerAPI.js
+++ server/routers/api/qrillerAPI.js
@@ -9,11 +9,26 @@ const auth_router = require(path.join(__dirname, "../auth_router.js"));
 const router = express.Router()
 
 // fetches questions in a specific document
-router.get("/db/:documentId", (req, res) => {
+router.get("/db/:documentId/qn", (req, res) => {
 	// return a js script to be runned in the browser
 	if (req.params.documentId in mem) {
 		var qriller = mem[req.params.documentId]
 		var plaintextqns = qriller.questions.map(qnObj => [qnObj.question, qnObj.equationList])
+
+		res.type("text/javascript")
+		res.write(`var QRILLER_ID = "${req.params.documentId}"; var QUESTIONS = ${JSON.stringify(plaintextqns)};`)
+		return res.status(200).end()
+	}
+
+	return res.status(404).end()
+})
+
+// fetches answers
+router.get("/db/:documentId/ans", (req, res) => {
+	// return a js script to be runned in the browser
+	if (req.params.documentId in mem) {
+		var qriller = mem[req.params.documentId]
+		var plaintextqns = qriller.questions.map(qnObj => qnObj.answer)
 
 		res.type("text/javascript")
 		res.write(`var QRILLER_ID = "${req.params.documentId}"; var QUESTIONS = ${JSON.stringify(plaintextqns)};`)
