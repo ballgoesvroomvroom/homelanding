3899d3627160e8f67b898421f5701598f0704133
whiteball <chongjayden07@gmail.com>
2023-06-06 20:22:41 +0800

    interface works, updated descriptions for percentage worksheets

--- server/routers/views/qrillerInterface.js
+++ server/routers/views/qrillerInterface.js
@@ -127,7 +127,7 @@ presetRouter.get("/perctofrac", (req, res) => {
 	// generate qriller object
 	var qrillerObj = new qriller.Qriller()
 	qrillerObj.title = "[2.2] Percentage to Fractions"
-	qrillerObj.note = "Express your answer in the simplest form, without mixed fractions."
+	qrillerObj.note = "Expressing percentage as a fraction.\nExpress your answer in the simplest form, without mixed fractions."
 
 	// attach new questions
 	qrillerObj.createQuestions(qriller.PercToFrac, 100)
@@ -143,19 +143,10 @@ presetRouter.get("/perctofrac", (req, res) => {
 	res.status(200).end()
 })
 
-presetRouter.get("/percchange", (req, res) => {
-	return Utils.generateQriller(
-		res,
-		"[2.3.4] Percentage Change Word Problems", "Round off your answer to 3 significant figures wherever possible.",
-		qriller.PercChange,
-		100,
-		true)
-})
-
 presetRouter.get("/percincr", (req, res) => {
 	return Utils.generateQriller(
 		res,
-		"[2.3.1] Percentage Increase", "Round off your answer to 3 significant figures wherever possible.",
+		"[2.3.1] Percentage Increase", "Calculate percentage increase from the given scenarios.\nRound off your answer to 3 significant figures wherever possible.",
 		qriller.PercChange,
 		100,
 		true,
@@ -165,7 +156,7 @@ presetRouter.get("/percincr", (req, res) => {
 presetRouter.get("/percdecr", (req, res) => {
 	return Utils.generateQriller(
 		res,
-		"[2.3.2] Percentage Decrease", "Round off your answer to 3 significant figures wherever possible.",
+		"[2.3.2] Percentage Decrease", "Calculate percentage decrease from the given scenarios.\nRound off your answer to 3 significant figures wherever possible.",
 		qriller.PercChange,
 		100,
 		true,
@@ -175,66 +166,60 @@ presetRouter.get("/percdecr", (req, res) => {
 presetRouter.get("/percchangeraw", (req, res) => {
 	return Utils.generateQriller(
 		res,
-		"[2.3.3] Percentage Change", "Round off your answer to 3 significant figures wherever possible.",
+		"[2.3.3] Percentage Change", "Calculate percentage change between 2 numbers.\nRound off your answer to 3 significant figures wherever possible.",
 		qriller.PercChange,
 		100,
 		false)
 })
-presetRouter.get("/percchangerawflip", (req, res) => {
+
+presetRouter.get("/percchange", (req, res) => {
 	return Utils.generateQriller(
 		res,
-		"[2.3.5] Percentage ", "Round off your answer to 3 significant figures wherever possible.",
+		"[2.3.4] Percentage Change Word Problems", "Calculate percentage change from the given word problems.\nRound off your answer to 3 significant figures wherever possible.",
 		qriller.PercChange,
 		100,
-		false,
-		null,
 		true)
 })
-presetRouter.get("/percchangerawflip", (req, res) => {
-	return Utils.generateQriller(
-		res,
-		"[2.3.5] Percentage Change Part 2", "Round off your answer to 3 significant figures wherever possible.",
-		qriller.PercChange,
-		100,
-		false,
-		null,
-		true)
-})
+
 presetRouter.get("/relpercincr", (req, res) => {
 	return Utils.generateQriller(
 		res,
-		"[2.4.1] Percentage Increase", "Round off your answers to 3 significant figures wherever possible.",
+		"[2.4.1] Percentage Manipulation [Increase]", "Increase a number by a factor relative to itself.\nRound off your answers to 3 significant figures wherever possible.",
 		qriller.RelativePercManipulation,
 		100,
 		true)
 })
+
 presetRouter.get("/relpercdecr", (req, res) => {
 	return Utils.generateQriller(
 		res,
-		"[2.4.2] Percentage Decrease", "Round off your answers to 3 significant figures wherever possible.",
+		"[2.4.2] Percentage Manipulation [Decrease]", "Decrease a number by a factor relative to itself.\nRound off your answers to 3 significant figures wherever possible.",
 		qriller.RelativePercManipulation,
 		100,
 		false)
 })
+
 presetRouter.get("/expunitperc", (req, res) => {
 	return Utils.generateQriller(
 		res,
-		"[2.5] Express Units", "Round off your answers to 3 significant figures wherever possible.",
+		"[2.5] Percentage Express Relative Units", "Express units relative to one another.\nRound off your answers to 3 significant figures wherever possible.",
 		qriller.ExpressUnitPerc,
 		100,
 		true)
 })
+
 presetRouter.get("/relperc", (req, res) => {
 	return Utils.generateQriller(
 		res,
-		"[2.6] Relative Percentage", "Round off your answers to 3 significant figures wherever possible.",
+		"[2.6] Percentage Of", "Calculate a result based on the percentage of a number.\nRound off your answers to 3 significant figures wherever possible.",
 		qriller.RelativePerc,
 		100)
 })
+
 presetRouter.get("/revperc", (req, res) => {
 	return Utils.generateQriller(
 		res,
-		"[2.7] Reverse Percentage", "Round off your answers to 3 significant figures wherever possible.",
+		"[2.7] Reverse Percentage", "Find a number given based on its percentage part.\nRound off your answers to 3 significant figures wherever possible.",
 		qriller.ReversePerc,
 		100)
 })
